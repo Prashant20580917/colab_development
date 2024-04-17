0f7ec3656b9be799c3f233f9ea8dc8e0842fb0d5
Rajat Pandit <razatp2203@gmail.com>
2024-04-17 13:05:14 +0545

    Chef code merged

--- restaurant/resources/views/foodchef.blade.php
+++ restaurant/resources/views/foodchef.blade.php
@@ -1,12 +1,15 @@
  <!-- ***** Chefs Area Starts ***** -->
+ 
  <section class="section" id="chefs">
         <div class="container">
             <div class="row">
                 <div class="col-lg-4 offset-lg-4 text-center">
+
                     <div class="section-heading">
                         <h6>Our Chefs</h6>
                         <h2>We offer the best ingredients for you</h2>
                     </div>
+
                 </div>
             </div>
 
