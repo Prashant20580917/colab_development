fc5ddf96dab10467ff9eded9ffa1f5f522907a7f
Rajat Pandit <razatp2203@gmail.com>
2024-05-16 11:17:31 +0545

    User cart backend code.

--- restaurant/app/Http/Controllers/HomeController.php
+++ restaurant/app/Http/Controllers/HomeController.php
@@ -50,5 +50,83 @@ class HomeController extends Controller
             return view('home',compact('data','data2','count'));
         }
     }
-    
+    public function addcart(Request $request,$id)
+    {
+        if (Auth::id())
+        {
+            $user_id=Auth::id();
+
+            $foodid=$id;
+            
+            $quantity=$request->quantity;
+
+            $cart=new cart;
+
+            $cart->user_id=$user_id;
+
+            $cart->food_id=$foodid;
+
+            $cart->quantity=$quantity;
+
+            $cart->save();
+
+            
+
+            return redirect()->back();
+        }
+        else{
+            return redirect('/login');
+        }
+    }
+
+    public function showcart(Request $request,$id)
+    {
+        $count=cart::where('user_id',$id)->count();
+
+        if(Auth::id()==$id)
+        {
+
+   
+
+        $data2=cart::select('*')->where('user_id','=',$id)->get();
+
+        $data=cart::where('user_id',$id)->join('food','carts.food_id','=','food.id')->get();
+
+        return view('showcart',compact('count','data','data2'));
+    }
+    else
+    {
+        return redirect()->back();
+    }
+    }
+
+    public function remove($id)
+    {
+        $data=cart::find($id);
+
+        $data->delete();
+        
+        return redirect()->back(); 
+    }
+
+    public function orderconfirm(Request $request)
+    {
+        foreach($request->foodname as $key=>$foodname);
+        {
+            $data=new order;
+            $data->foodname=$foodname;
+            $data->price=$request->price[$key];
+            $data->quantity=$request->quantity[$key];
+            $data->name=$request->name;
+            $data->phone=$request->phone;
+            $data->address=$request->address;
+
+            $data->save();
+
+        }
+        return redirect()->back();
+
+    }
+
+
 }
